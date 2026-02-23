04b2795dd2c8ff9b55de40b266f3023ca5b07e37
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-02-23 23:37:41 +0500

    adres added

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,38 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
       
-      <h1 className="text-3xl font-bold text-center mb-8">
+      <h1 className="text-3xl font-bold text-center mb-10">
         Product List
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
         
         <Card 
           image="https://i.pinimg.com/236x/62/9c/c7/629cc724344431095326fb0de49a3d82.jpg" 
-          title="iPhone 16 Pro Max"
+          title="iPhone 17 Pro Max"
+          price="1400"
+          address="shalimaar gali , quetta"
         />
 
         <Card 
           image="https://images.pexels.com/photos/3550482/pexels-photo-3550482.jpeg" 
           title="HP Laptop ThinkPad"
+          price="2400"
+          address="jinnah town , lahor"
         />
 
         <Card 
           image="https://images.pexels.com/photos/29020349/pexels-photo-29020349.jpeg" 
-          title="iPhone 16 Pro Max"
-        /> 
-         <Card 
-          image="https://images.pexels.com/photos/1334597/pexels-photo-1334597.jpeg?_gl=1*15aast8*_ga*Nzc2MzgxNTg0LjE3NjU0ODUxMTE.*_ga_8JE65Q40S6*czE3NzE4Njg2NTMkbzIkZzEkdDE3NzE4NzAxOTgkajU5JGwwJGgw" 
-          title="Apple Ipad 25"
+          title="iPhone 15 Pro Max"
+          price="1600" address="quetta , balochistan"
+        />
+
+        <Card 
+          image="https://images.pexels.com/photos/1334597/pexels-photo-1334597.jpeg" 
+          title="Apple iPad 25" price="1820" address="karachi , pakistan"
         />
 
       </div>
-
     </div>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 
-function Card({ image, title }) {
+function Card(props) {
   return (
-    <div className="bg-white rounded-2xl shadow-lg overflow-hidden 
-                    hover:shadow-2xl hover:-translate-y-2 
-                    transition duration-300 w-75">
+    <div className="bg-white rounded-2xl shadow-md overflow-hidden
+                    hover:shadow-xl hover:-translate-y-2 
+                    transition duration-300 flex flex-col">
 
       {/* Image */}
-      <div className="overflow-hidden">
-        <img
-          src={image}
-          alt={title}
-          className="w-full  object-cover hover:scale-105 transition duration-300"
-        />
-      </div>
+      <img
+        src={props.image}
+        alt={props.title}
+        className="w-full h-72 object-cover"
+      />
 
       {/* Content */}
-      <div className="p-5">
-        <h2 className="text-lg font-bold text-gray-800 mb-3">
-          {title}
+      <div className="p-5 flex flex-col gap-2 flex-grow">
+
+        <h2 className="text-lg font-bold text-gray-800">
+          {props.title}
         </h2>
 
-        <button className="w-full bg-blue-600 text-white py-2 rounded-lg 
-                           hover:bg-blue-700 transition duration-300">
-          View Details
+        <h3 className="text-blue-600 text-xl font-semibold">
+          ${props.price}
+        </h3>
+
+        <p className="text-gray-500 text-sm">
+          📍 {props.address}
+        </p>
+
+        <button className="mt-auto bg-black text-white py-2 rounded-lg
+                           hover:bg-gray-800 transition">
+          Buy Now
         </button>
+
       </div>
 
     </div>
