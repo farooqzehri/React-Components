2a3a46c60b2bb0dc20e1e17d99c01e28d7dd6795
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-02-23 23:19:13 +0500

    added card css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import React, { useState } from 'react'
+import React from "react";
+import Card from "./components/card";
+
 function App() {
-  const [data , setData] = useState([])
   return (
-    <>
-    <h1>Hello World.</h1>
-    </>
-    
-  )
+    <div className="min-h-screen bg-gray-100 p-8">
+      
+      <h1 className="text-3xl font-bold text-center mb-8">
+        Product List
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        
+        <Card 
+          image="https://i.pinimg.com/236x/62/9c/c7/629cc724344431095326fb0de49a3d82.jpg" 
+          title="iPhone 16 Pro Max"
+        />
+
+        <Card 
+          image="https://images.pexels.com/photos/3550482/pexels-photo-3550482.jpeg" 
+          title="HP Laptop ThinkPad"
+        />
+
+        <Card 
+          image="https://images.pexels.com/photos/29020349/pexels-photo-29020349.jpeg" 
+          title="iPhone 16 Pro Max"
+        /> 
+         <Card 
+          image="https://images.pexels.com/photos/1334597/pexels-photo-1334597.jpeg?_gl=1*15aast8*_ga*Nzc2MzgxNTg0LjE3NjU0ODUxMTE.*_ga_8JE65Q40S6*czE3NzE4Njg2NTMkbzIkZzEkdDE3NzE4NzAxOTgkajU5JGwwJGgw" 
+          title="Apple Ipad 25"
+        />
+
+      </div>
+
+    </div>
+  );
 }
-export default App
+
+export default App;
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,8 +1,34 @@
-function card(props){
-    return(
-        <div>
-            <img src={props.image} alt={props.alt} />
-            <h2>{props.disciption}</h2>
-        </div>
-    )
+import React from "react";
+
+function Card({ image, title }) {
+  return (
+    <div className="bg-white rounded-2xl shadow-lg overflow-hidden 
+                    hover:shadow-2xl hover:-translate-y-2 
+                    transition duration-300 w-75">
+
+      {/* Image */}
+      <div className="overflow-hidden">
+        <img
+          src={image}
+          alt={title}
+          className="w-full  object-cover hover:scale-105 transition duration-300"
+        />
+      </div>
+
+      {/* Content */}
+      <div className="p-5">
+        <h2 className="text-lg font-bold text-gray-800 mb-3">
+          {title}
+        </h2>
+
+        <button className="w-full bg-blue-600 text-white py-2 rounded-lg 
+                           hover:bg-blue-700 transition duration-300">
+          View Details
+        </button>
+      </div>
+
+    </div>
+  );
 }
+
+export default Card;
